24394f0725a58cc88f6fb125505920e5cea8436e
Nishant Kumar Singh <nishant.itbhu@hotmail.com>
2019-02-28 07:39:28 +0000

    Add IsIterator to TypeTraits

--- code/include/TypeTraits.h
+++ code/include/TypeTraits.h
@@ -11,5 +11,11 @@ struct IsTemplateInstanceOf : std::false_type {};
 template<template<typename...> typename U, typename... args>
 struct IsTemplateInstanceOf<U, U<args...>> : std::true_type {};
 
+template<typename T, typename = void>
+struct IsIterator : std::false_type {};
+
+template<typename T>
+struct IsIterator<T, std::void_t<std::iterator_traits<T>::iterator_category>> : std::true_type {};
+
 }  // namespace core
 }  // namespace nxt
