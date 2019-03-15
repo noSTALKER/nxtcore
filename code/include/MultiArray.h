2f7a5e1cd766ef7981bf3d613edff460820219dc
Nishant Kumar Singh <nishant.itbhu@hotmail.com>
2019-03-15 07:00:19 +0000

    Add Multiarray class

---
+++ code/include/MultiArray.h
@@ -0,0 +1,57 @@
+#include <array>
+#include <iostream>
+
+template <typename T, size_t... Ns>
+class multi_array;
+
+template <typename T, size_t N0, size_t... Ns>
+class multi_array<T, N0, Ns...> {
+public:
+    using internal_type = typename std::array<multi_array<T, Ns...>, N0>;
+    using reference = typename internal_type::reference;
+    using const_reference = typename internal_type::const_reference;
+
+    reference operator[](size_t index) {
+        return array_[index];
+    }
+
+    constexpr size_t size() const {
+        return array_.size();
+    }
+
+    template<typename SizeT, typename... Args>
+    auto operator()(SizeT i, Args... args) {
+        static_assert(std::is_convertible_v<SizeT, size_t>, "Invalid index type");
+        return array_[i](args...);
+    }
+
+    template<typename SizeT>
+    auto operator()(SizeT i) {
+        static_assert(std::is_convertible_v<SizeT, size_t>, "Invalid index type");
+        return array_[i];
+    }
+
+private:
+    internal_type array_;
+};
+
+template <typename T, size_t N0>
+class multi_array<T, N0> {
+public:
+    using internal_type = typename std::array<T, N0>;
+    using reference = typename internal_type::reference;
+    reference operator[](size_t index) {
+        return array_[index];
+    }
+
+    constexpr size_t size() const {
+        return array_.size();
+    }
+
+    reference operator()(size_t index) {
+        return array_[index];
+    }
+
+private:
+    internal_type array_;
+};
